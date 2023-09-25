d53f2eacab92539da1c2877ba16a02eb63d88dfc
Alex Harris <alexharris@Alexs-MacBook-Air.local>
2023-09-24 20:04:36 -0700

    add prints

--- pages/shop.js
+++ pages/shop.js
@@ -44,6 +44,38 @@ class Shop extends React.Component {
       <div>
         <h1>Shop</h1>
         <ReactMarkdown>{this.state.modifiedContent.shop_intro}</ReactMarkdown>
+
+        <div class="p-12 mb-16 flex flex-col md:flex-row gap-8 bg-gray-100">
+          <div class="w-1/2">
+            <img src="bolm-print.jpg" />
+          </div>
+          <div class="w-1/2">
+            <h2 class="text-4xl">Common Birds of Lake Merritt Fine Art Print</h2>
+            <p>A fine art print identifying 19 of the most common birds at Lake Merritt, beautifully printed on Signature Worthy® Cold Press paper and signed by the artist.</p>
+            <p>Currently available in two sizes:</p>
+            <p class="text-2xl">12 x 16 - $50</p>
+            <script async
+  src="https://js.stripe.com/v3/buy-button.js">
+</script>
+
+<stripe-buy-button
+  buy-button-id="buy_btn_1Nu4ndEB2SNh31gwdBLrBZRt"
+  publishable-key="pk_live_7ijeS9mbS8G6IBtOCHOmPK9p"
+>
+</stripe-buy-button> 
+            <p class="text-2xl mt-8">16 x 20 - $75</p>
+            <script async
+  src="https://js.stripe.com/v3/buy-button.js">
+</script>
+
+<stripe-buy-button
+  buy-button-id="buy_btn_1Nu4lhEB2SNh31gwYhqndds0"
+  publishable-key="pk_live_7ijeS9mbS8G6IBtOCHOmPK9p"
+>
+</stripe-buy-button>
+          </div>
+        </div>
+        <p>Some prints and paitings are also available at stores in the East Bay:</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 items-start">
           <Card 
             image="shop-gardener.jpg" 
